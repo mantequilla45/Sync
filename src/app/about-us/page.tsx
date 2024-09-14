6c5d366b5e7d2184e3bfe783ce9bdfa2ee110a65
Vince <152976699+mantequilla45@users.noreply.github.com>
2024-09-14 20:13:33 +0800

    replaced the signup function

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,13 @@
+
+import Header from '../../components/protected/header';
+
+const BlankPage = () => {
+    return (
+      <div>
+        <Header/>
+        <h1>Blank Page</h1>
+      </div>
+    );
+  };
+  
+  export default BlankPage;
