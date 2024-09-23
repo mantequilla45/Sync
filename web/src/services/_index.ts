25b1d5af0ea415a0e36390cbe828a938e2495f4f
rhongobongo <148690138+rhongobongo@users.noreply.github.com>
2024-09-23 05:33:25 +0000

    WIP: Service for document creation

---
+++ web/src/services/_index.ts
@@ -0,0 +1,4 @@
+import { createProject } from "./project/createProject";
+import { createDocument } from "./document/createDocument";
+
+export { createProject, createDocument }; // Re-export the createProject function
---
+++ web/src/services/document/createDocument.ts
@@ -0,0 +1,25 @@
+export const createDocument = async (formData: FormData, setError: (error: string) => void): Promise<void> => {
+    try {
+      const response = await fetch('/api/Project/createDocument', { // Ensure this is the correct endpoint
+        method: 'POST',
+        body: formData,
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || 'Failed to create project');
+      }
+  
+      await response.json(); // Process the successful response as needed
+    } catch (error: unknown) {
+        // Type assertion to ensure `error` is of type `Error`
+        if (error instanceof Error) {
+          console.error('Error creating project:', error);
+          setError(error.message); // Set the error message
+        } else {
+          console.error('Unexpected error:', error);
+          setError('An unexpected error occurred'); // Fallback for unknown error types
+        }
+  }
+  
+}
