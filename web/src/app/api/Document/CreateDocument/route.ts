d2baec0f83560a1eadcb59a605b2d9228d78172e
rhongobongo <148690138+rhongobongo@users.noreply.github.com>
2024-09-23 11:36:04 +0800

    WIP: groundwork for document editing, Next API side

--- web/src/app/api/Document/CreateDocument/route.ts
+++ web/src/app/api/Document/CreateDocument/route.ts
@@ -1,23 +1,61 @@
 import { NextResponse } from 'next/server';
+import { Document } from '../../../../../../_shared/interface';
+import { db } from '@/lib/Firebase/_index';
+import { FieldValue } from 'firebase-admin/firestore';
 
 export async function POST(req: Request) {
   try {
-
-    const userId =  req.headers.get('x-user-id');
-
+    const userId = req.headers.get('x-user-id');
     if (!userId) {
       return NextResponse.json({ error: 'User ID is missing' }, { status: 400 });
     }
 
     const formData = await req.formData();
-    const file = formData.get('name') as File;
+
+    const projectId = formData.get('projectId');
+    const documentTitle = formData.get('title');
+    const filePath = formData.get('filePath');
+
+    if (!projectId || !documentTitle || !filePath) {
+      return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+    }
+
+    const projectRef = db.collection('projects').doc(projectId as string);
+    const projectSnapshot = await projectRef.get();
+    if (!projectSnapshot.exists) {
+      return NextResponse.json({ error: 'Project not found' }, { status: 404 });
+    }
 
 
+    const newDocument: Document = {
+      UID: '',
+      title: documentTitle as string,
+      filePath: filePath as string,
+      createdBy: userId,
+      lastEditedBy: userId,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      collaboratorUIDs: [],
+      activeUserUIDs: [],
+      versionHistoryUIDs: [],
+      permissionUIDs: [],
+      commentUIDs: [],
+      taskUIDs: []
+    };
+
+    const documentRef = db.collection('documents').doc();
+    newDocument.UID = documentRef.id;
 
 
-    return NextResponse.json({ status: 200 });
+    await documentRef.set(newDocument, { merge: true });
+
+    await projectRef.update({
+      documentUIDs: FieldValue.arrayUnion(newDocument.UID)
+    });
+
+    return NextResponse.json({ status: 200, documentId: newDocument.UID });
   } catch (error) {
-    console.error('Error uploading file:', error);
-    return NextResponse.json({ error: 'Failed to upload file' }, { status: 500 });
+    console.error('Error creating document:', error);
+    return NextResponse.json({ error: 'Failed to create document' }, { status: 500 });
   }
 }
--- web/src/app/api/Project/createProject/route.ts
+++ web/src/app/api/Project/createProject/route.ts
@@ -1,23 +1,44 @@
 import { NextResponse } from 'next/server';
+import { Project} from '../../../../../../_shared/interface'
+import { db } from '@/lib/Firebase/_index';
 
 export async function POST(req: Request) {
   try {
-
-    const userId =  req.headers.get('x-user-id');
+    const userId = req.headers.get('x-user-id');
 
     if (!userId) {
       return NextResponse.json({ error: 'User ID is missing' }, { status: 400 });
     }
 
     const formData = await req.formData();
-    //const file = formData.get('name') as File;
-    
-    
 
 
-    return NextResponse.json({ status: 200 });
+    const projectName = formData.get('name');
+    const projectDescription = formData.get('description');
+
+    if (!projectName || !projectDescription) {
+      return NextResponse.json({ error: 'Project name or description is missing' }, { status: 400 });
+    }
+
+    const newProject: Project = {
+      UID: '',
+      title: projectName as string,
+      description: projectDescription as string,
+      owner: userId, // The user ID of the owner
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      userRoleUIDs: [], 
+      documentUIDs: [], 
+      taskUIDs: [] 
+    };
+
+    const projectRef = db.collection("projects").doc();
+    newProject.UID = projectRef.id;
+    await projectRef.set(newProject, { merge: true });
+
+    return NextResponse.json({ status: 200, projectId: newProject.UID });
   } catch (error) {
-    console.error('Error uploading file:', error);
-    return NextResponse.json({ error: 'Failed to upload file' }, { status: 500 });
+    console.error('Error uploading project:', error);
+    return NextResponse.json({ error: 'Failed to upload project' }, { status: 500 });
   }
 }
