25e430f9f1f165e784113b5022f234a3411d54be
rhongobongo <148690138+rhongobongo@users.noreply.github.com>
2024-10-14 21:42:59 +0800

    fix: firebase admin env middleware side

--- web/src/lib/Firebase/FirebaseAdmin.ts
+++ web/src/lib/Firebase/FirebaseAdmin.ts
@@ -1,8 +1,4 @@
 import * as admin from 'firebase-admin';
-import * as dotenv from 'dotenv';
-import { resolve } from 'path';
-
-dotenv.config({ path: resolve(process.cwd(), '../.env') });
 
 if (!admin.apps.length) {
   admin.initializeApp({
