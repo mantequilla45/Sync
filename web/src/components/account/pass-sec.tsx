24cf4d1b5789eae4bebb1093c6c71ac7e6863815
Vince Kevin Añana <152976699+mantequilla45@users.noreply.github.com>
2024-10-31 17:26:08 +0800

    fixed profile card
    separated each card into their own component

---
+++ web/src/components/account/pass-sec.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PasswordSecurity = () => {
+  return (
+    <div className="flex-1 bg-white rounded-xl shadow-md p-5">
+        <h2 className="text-2xl text-[#69369B] font-semibold mb-4">Password and Security</h2>
+        <p className="text-gray-700">This is some content inside the second card.</p>
+    </div>
+  );
+};
+
+export default PasswordSecurity;
